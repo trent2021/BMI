f2c18150cd1ceb635982f6e2c00c8634eec04f9c
Trent <trent.polenchar.2021@student.lhschools.org>
2019-02-04 14:17:30 -0500

    Declared int

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,8 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner keyboard;
+        int feet;
+        int inches;
 
         keyboard = new Scanner(System.in);
+
+        System.out.println("What is your height? (Only feet)");
+        feet = keyboard.nextLine();
+
+        System.out.println("What is your height? (Only inches)");
+        inches = keyboard.nextLine();
+
+
+        System.out.println(feet);
+        System.out.println(inches);
+        System.out.println(feet*inches);
+
+
 
 
 
