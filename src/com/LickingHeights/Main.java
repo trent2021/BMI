912fba909cafa9dfc9c5742683f8cc42418a97c9
Trent <trent.polenchar.2021@student.lhschools.org>
2019-02-04 13:53:35 -0500

    Declared scanner

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,15 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner keyboard;
+
+        keyboard = new Scanner(System.in);
+
+
+
     }
 }
