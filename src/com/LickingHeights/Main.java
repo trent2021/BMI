dd6c8ab5b097c8f6ff393ea26157326e232a00cd
Trent <trent.polenchar.2021@student.lhschools.org>
2019-02-05 13:57:21 -0500

    Added BMI equation

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,6 +8,10 @@ public class Main {
         Scanner keyboard;
         int feet;
         int inches;
+        int pounds;
+        double kilograms;
+        double meters;
+        double BMI;
 
         keyboard = new Scanner(System.in);
 
@@ -17,14 +21,16 @@ public class Main {
         System.out.println("What is your height? (Only inches)");
         inches = keyboard.nextInt();
 
+        System.out.println("What is your weight? (In pounds)");
+        pounds = keyboard.nextInt();
+
         inches = inches + (feet * 12);
+        meters = inches * 0.0254;
+        kilograms = pounds * 0.453592;
 
+        BMI = kilograms/(meters * meters);
 
-
-
-
-
-
-
+        System.out.println("Your BMI is ");
+        System.out.print(BMI);
     }
 }
